205457d952f8890e2cc64a3cc5bc660fa77dc5f3
lhawes <lucashawes@Lucass-MacBook-Pro.local>
2020-08-17 12:34:40 -0700

    adding more emotion themeing

--- src/components/shared/Block/Block.tsx
+++ src/components/shared/Block/Block.tsx
@@ -1,13 +1,31 @@
-import * as React from 'react';
-import { css } from 'emotion';
+// import * as React from 'react';
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import { useMemo } from 'react';
 
-const blockCss = css`
-  display: inline-block,
-`;
+const baseBlockCss = {
+}
 
-export const Block: React.FC = ({ children }) => {
+export interface BlockProps {
+  cssOverrides?: {
+    flex?: number
+  },
+}
+
+export const BlockCssDefault = {
+  flex: 1
+}
+
+export const Block: React.FC<BlockProps> = ({ children, cssOverrides = BlockCssDefault }) => {
+  const computedCss = useMemo(() => {
+    return {
+      ...baseBlockCss,
+      ...cssOverrides
+    }
+  },[cssOverrides])
+
   return (
-    <div className={blockCss}>
+    <div css={computedCss}>
       { children }
     </div>
   );
--- src/components/shared/Section/Section.tsx
+++ src/components/shared/Section/Section.tsx
@@ -4,6 +4,9 @@ import { css } from 'emotion';
 const sectionCss = css`
   margin: 10px 0;
   padding: 0 10px;
+  display: flex;
+  flex-direction: row;
+  align-items: stretch;
 `;
 
 export const Section: React.FC = ({ children }) => {
