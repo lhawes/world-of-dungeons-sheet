a44fc11352c7dba8aa62f0bb169c9e1130c31db7
lhawes <lucashawes@Lucass-MacBook-Pro.local>
2020-08-18 09:30:49 -0700

    finish gray and white box styling

--- src/components/shared/Block/Block.tsx
+++ src/components/shared/Block/Block.tsx
@@ -1,15 +1,36 @@
 // import * as React from 'react';
 /** @jsx jsx */
-import { jsx } from '@emotion/core'
+import { jsx, css } from '@emotion/core'
 import { useMemo } from 'react';
+import topLeftSvg from '../../../static/svg/gray-top-left-scoop.svg';
+import topRightSvg from '../../../static/svg/gray-top-right-scoop.svg';
+import bottomLeftSvg from '../../../static/svg/gray-bottom-left-scoop.svg';
+import bottomRightSvg from '../../../static/svg/gray-bottom-right-scoop.svg';
 
-const baseBlockCss = {
-  background: 'white',
-}
+
+const backgroundWhite = 'white';
+const grayScoopSize = '10px';
+const grayScoopOffset = '10px';
+
+const grayCornerScoopsCss = css`
+  background-image: url(${topLeftSvg}), url(${topRightSvg}), url(${bottomLeftSvg}), url(${bottomRightSvg});
+  background-color: ${backgroundWhite};
+  background-size: ${grayScoopSize} ${grayScoopSize};
+  background-repeat: no-repeat;
+  background-position: 0 0, 100% 0, 0 100%, 100% 100%;
+  padding: ${grayScoopOffset};
+`;
+
+const baseBlockCss = css`
+  ${grayCornerScoopsCss}
+  :not(:last-child) {
+    margin-right: 7px;
+  }
+`;
 
 export interface BlockProps {
   cssOverrides?: {
-    flex?: number
+    flex: number
   },
 }
 
@@ -19,10 +40,10 @@ export const BlockCssDefault = {
 
 export const Block: React.FC<BlockProps> = ({ children, cssOverrides = BlockCssDefault }) => {
   const computedCss = useMemo(() => {
-    return {
-      ...baseBlockCss,
-      ...cssOverrides
-    }
+    return css`
+      ${baseBlockCss}
+      flex: ${cssOverrides.flex};
+    `
   },[cssOverrides])
 
   return (
--- src/components/shared/Section/Section.tsx
+++ src/components/shared/Section/Section.tsx
@@ -1,22 +1,30 @@
 import * as React from 'react';
 import { css } from 'emotion';
-import topLeftSvg from '../../../static/svg/top-left-scoop.svg';
-import topRightSvg from '../../../static/svg/top-right-scoop.svg';
-import bottomLeftSvg from '../../../static/svg/bottom-left-scoop.svg';
-import bottomRightSvg from '../../../static/svg/bottom-right-scoop.svg';
+import topLeftSvg from '../../../static/svg/white-top-left-scoop.svg';
+import topRightSvg from '../../../static/svg/white-top-right-scoop.svg';
+import bottomLeftSvg from '../../../static/svg/white-bottom-left-scoop.svg';
+import bottomRightSvg from '../../../static/svg/white-bottom-right-scoop.svg';
 
+const backgroundGray = '#c8c8c8';
+const whiteScoopSize = '10px';
+const whiteScoopOffset = '7px';
+
+const whiteCornerScoopsCss = css`
+  background-image: url(${topLeftSvg}), url(${topRightSvg}), url(${bottomLeftSvg}), url(${bottomRightSvg});
+  background-color: ${backgroundGray};
+  background-size: ${whiteScoopSize} ${whiteScoopSize};
+  background-repeat: no-repeat;
+  background-position: 0 0, 100% 0, 0 100%, 100% 100%;
+  padding: ${whiteScoopOffset};
+`;
 
 const sectionCss = css`
+  ${whiteCornerScoopsCss};
   margin: 0;
-  padding: 10px;
   display: flex;
   flex-direction: row;
   align-items: stretch;
-  background-image: url(${topLeftSvg}), url(${topRightSvg}), url(${bottomLeftSvg}), url(${bottomRightSvg});
-  background-color: #c8c8c8;
-    background-size: 10px 10px;
-    background-repeat: no-repeat;
-    background-position: 0 0, 100% 0, 0 100%, 100% 100%;
+  
 `;
 
 export const Section: React.FC = ({ children }) => {
